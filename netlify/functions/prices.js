0a54a74ad5a92d61dd322ddd6cb2fd7828ed98d6
lennyscheuer-gif <lennyscheuer@gmail.com>
2026-04-25 15:35:24 +0200

    Update prices.js

--- netlify/functions/prices.js
+++ netlify/functions/prices.js
@@ -33,15 +33,13 @@ function fetchPriceLabs(body) {
 
 exports.handler = async function(event, context) {
   try {
-    const ids = ['1854001', '11854001', '1185400101', '11854001___1185400101'];
-    const pmsOptions = ['booking', 'Booking.com'];
+    const id = '894682312121769350';
+    const pmsOptions = ['airbnb', 'Airbnb', 'airbnb.com'];
     const results = {};
 
-    for (const id of ids) {
-      for (const pms of pmsOptions) {
-        const r = await fetchPriceLabs({ listings: [{ id, pms }] });
-        results[`${id} / ${pms}`] = { status: r.status, preview: JSON.stringify(r.data).substring(0, 200) };
-      }
+    for (const pms of pmsOptions) {
+      const r = await fetchPriceLabs({ listings: [{ id, pms }] });
+      results[`${id} / ${pms}`] = { status: r.status, preview: JSON.stringify(r.data).substring(0, 300) };
     }
 
     return {
